334456546870e41d657c8bda1ce53cebd5c9c7cd
saurabhverma <srvvrma0@gmail.com>
2018-07-29 15:37:25 +0530

    Model window Added
    Model window added of while short link

--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
-$(function(){
-
-	$('.probootstrap-toggle').on('click', function(){
-		// console.log('nice');
-
-		var mainNav = $('.probootstrap-main-nav');
-
-		
-			if ( !mainNav.hasClass('active') ) {
-				
-					mainNav.addClass('active');
-	
-			} else {
-				mainNav.removeClass('active');
-			
-			}
-	});
-
-});
-
-
+var ERROR = 'error';
+var baseURL = 'http://srt.bz/';
 function isValidURL(url){
     var pattern = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     if(pattern.test(url)){
@@ -27,7 +8,6 @@ function isValidURL(url){
         return false;
     }
 }
-
 function is_url(str)
 {
     regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
@@ -40,3 +20,101 @@ function is_url(str)
         return false;
     }
 }
+
+function generateURL() {
+    var url =  $('#urlInput').val();
+    if(!is_url(url)){
+        showNotification(ERROR,"Please provide a valid url.");
+        return false;
+    } else{
+        generateShortenURL(url);
+    }
+}
+
+function generateShortenURL(url) {
+    $('#targetURL').val(url)
+    createNotificationForURL();
+    $('#shortenURLHeader').text(baseURL + 'abcdef');
+    $('#shortenSufix').val('abcdef')
+    $('#shortenURLModel').modal('show');
+}
+
+function showNotification(type,msg) {
+
+    if(type == ERROR) {
+        iziToast.error({
+            id: 'error',
+            title: 'Error',
+            message: msg,
+            position: 'topRight',
+            transitionIn: 'fadeInDown'
+        });
+    }
+
+}
+
+function copyToClipBoard(){
+    iziToast.info({
+        id: 'info',
+        title: 'Info',
+        message: 'Copied to clipboard.',
+        position: 'topRight',
+        transitionIn: 'fadeInDown'
+    });
+}
+
+function createNotificationForURL() {
+
+    iziToast.show({
+        id: 'haduken',
+        theme: 'dark',
+        icon: 'icon-contacts',
+        title: 'Hey',
+        displayMode: 2,
+        message: 'Hi, Shorten URL has been generated successfully.',
+        position: 'bottomCenter',
+        transitionIn: 'flipInX',
+        transitionOut: 'flipOutX',
+        progressBarColor: 'rgb(0, 255, 184)',
+        image: 'img/avatar.jpg',
+        imageWidth: 70,
+        layout: 2,
+        image: 'images/qrcode.png',
+        balloon: true,
+        buttons: [
+            ['<button>Copy URL</button>',
+                function(instance, toast) {
+                    iziToast.show({
+                        theme: 'dark',
+                        icon: 'icon-photo',
+                        message: 'Copied to clipboard.',
+                        position: 'topRight',
+                        // iconText: 'star',
+                    });
+
+                },
+                true
+            ],
+            ['<button>Open URL</button>',
+                function(instance, toast) {
+
+                    iziToast.show({
+                        theme: 'dark',
+                        icon: 'icon-ondemand_video',
+                        message: 'Opening URL in New Tab.',
+                        position: 'topRight',
+                        // iconText: 'star',
+                    });
+
+                }
+            ]
+        ]
+    });
+
+}
+
+
+
+$( document ).ready(function() {
+    $('#urlGenerateBtn').click(generateURL);
+});
